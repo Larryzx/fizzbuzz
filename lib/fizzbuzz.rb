f29f2365d8db63a1fd4eb6596ca5a24b8ce682fa
Laurence Kite <laurent.kitecb@gmail.com>
2016-07-01 16:19:37 +0000

    Added fizzbuzz code and spce

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,14 @@
 def fizzbuzz(number)
-  'fizz'
-
+  if number.is_a? integer
+     "That is not an integer!"
+  end      
+  if number % 5 == 0 && number % 3 == 0
+    "fizzbuzz"
+  elsif number % 5 == 0
+    "buzz"
+  elsif number % 3 == 0
+    "fizz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,19 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "That is not an integer!" when past a string' do
+    expect(fizzbuzz("ten")).to eq 'That is not an integer!'
+  end    
+  it 'returns "buzz" when passed  a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizz" when passed a multiple of 3' do
+       expect(fizzbuzz(3)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+       expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns given number in any other case' do
+    expect(fizzbuzz(23)).to eq 23
+  end
+  
 end
